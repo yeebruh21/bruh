bd89a4c18f96de8b2dc2043072871757648da46e
Anthony <achen3741@gmail.com>
2021-03-14 12:56:32 -0400

    Initial Commit

---
+++ Brahski/BrahskiApp.swift
@@ -0,0 +1,17 @@
+//
+//  BrahskiApp.swift
+//  Brahski
+//
+//  Created by Anthony Chen on 2021-03-14.
+//
+
+import SwiftUI
+
+@main
+struct BrahskiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
